b66e4623ae9f0f7bcde633eafd04d37600ad2112
gimefi <401637852@qq.com>
2019-01-03 23:43:11 +0800

    start

---
+++ edit_file.py
@@ -0,0 +1,21 @@
+# code=utf8
+import sys
+import os
+
+def edit(file_path):
+    print "do your job to", file_path
+
+def travel(abs_path):
+    for sub_dir, sub_dir_of_subdir, files_in_sub_dir in os.walk(abs_path):
+        for single_file in files_in_sub_dir:
+            file_abs_path = os.path.join(sub_dir, single_file)
+            edit(file_abs_path)
+    
+def main():
+    for path in sys.argv[1:]:
+        print "this is the path you want to edit: ",path
+        travel(path)
+    print "finish"
+        
+if __name__ == "__main__":
+    main()
